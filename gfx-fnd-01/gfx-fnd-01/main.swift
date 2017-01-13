b5b10d02ff422c51cd9dbcb3e421957c1c640885
asu <arsenyk.ustaris.dev@gmail.com>
2017-01-12 21:19:30 -0800

    Create project for 01.  Create workspace to contain projects.

---
+++ gfx-fnd-01/gfx-fnd-01/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  gfx-fnd-01
+//
+//  Created by asu on 2017-01-12.
+//  Copyright © 2017 ArsenykUstaris. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
