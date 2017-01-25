1ea3481504a85629e1ed7b25c79f247db25424e1
asu <arsenyk.ustaris.dev@gmail.com>
2017-01-24 19:10:34 -0800

    New project for module 1

---
+++ gfx-fnd-10/gfx-fnd-10/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  gfx-fnd-10
+//
+//  Created by asu on 2017-01-23.
+//  Copyright © 2017 ArsenykUstaris. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
