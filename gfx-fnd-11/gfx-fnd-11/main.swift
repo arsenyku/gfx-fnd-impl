f10a6dd7f6c1273e13d9f8305dd995e7efa64fc0
asu <arsenyk.ustaris.dev@gmail.com>
2017-01-29 15:35:28 -0800

    Add boilerplate to 11 project

--- gfx-fnd-11/gfx-fnd-11/main.swift
+++ gfx-fnd-11/gfx-fnd-11/main.swift
@@ -8,5 +8,83 @@
 
 import Foundation
 
-print("Hello, World!")
+func Usage()
+{
+  let exec = CommandLine.arguments[0]
+  print ("\(exec.components(separatedBy: "/").last ?? exec) <inputfile> <outputfile>")
+}
+
+func readArgs() -> (inputFile:String, outputFile:String?)
+{
+  guard let inputFile = CommandLine.arguments[safe: 1]
+    else
+  {
+    Usage()
+    exit (1)
+  }
+  
+  if (!FileManager.default.fileExists(atPath: inputFile))
+  {
+    print ("Input file does not exist: \(inputFile)")
+    exit(1)
+  }
+  
+  let outputFile = CommandLine.arguments[safe:2]
+  
+  return (inputFile, outputFile)
+  
+}
+
+func parseInputJson(contents json:JSON) -> (background:Colour, width:Int, height:Int, triangles:[Shape])
+{
+  guard let jsonBg = json["background"].array?.map({ $0.int ?? 0 }),
+    let jsonHeight = json["height"].int,
+    let jsonWidth = json["width"].int,
+    let jsonTriangles = json["triangles"].array
+    else
+  {
+    print ("JSON content could not be parsed")
+    exit(1)
+  }
+  
+  let bgColour = Colour(jsonBg[0], jsonBg[1], jsonBg[2], Pixel.DEFAULT_MAX_GRAYSCALE)
+  let width = jsonWidth
+  let height = jsonHeight
+  let triangles = jsonTriangles.map({ jsonTriangle -> Shape? in
+    guard let jsonPoints = jsonTriangle["points"].array,
+      let jsonColour = jsonTriangle["color"].array?.map({ $0.int ?? 0 })
+      else
+    {
+      return nil
+    }
+    
+    let points = jsonPoints.map({ jsonPoint -> Point in
+      return Point(jsonPoint[0].floatValue,jsonPoint[1].floatValue)
+    })
+    
+    let colour = Colour(jsonColour[0], jsonColour[1], jsonColour[2], Pixel.DEFAULT_MAX_GRAYSCALE)
+    
+    return Shape(points:points, colour:colour)
+  })
+    .filter({ $0 != nil })
+    .map({ $0! })
+  
+  return (bgColour, width, height, triangles)
+}
+
+func main()
+{
+  let (inputFile, outputFile) = readArgs()
+  let jsonInput = JSON(data: read(pathAndFilename: inputFile).data(using: .ascii)!)
+  let (bg, width, height, triangles) = parseInputJson(contents: jsonInput)
+  
+  PNMImage(type: .RGB, width: width, height: height, maxScale: Pixel.DEFAULT_MAX_GRAYSCALE)
+    .paintRect(p1: (0,0), p2: (width-1, height-1), colour: bg)
+    .draw(triangles:triangles)
+    .write(toFile: outputFile)
+  
+}
+
+
+main()
 
