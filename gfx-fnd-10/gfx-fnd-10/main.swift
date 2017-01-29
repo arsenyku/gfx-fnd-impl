8e8d2f82a699aad72b3cc2207b24b31955686dec
asu <arsenyk.ustaris.dev@gmail.com>
2017-01-28 19:02:38 -0800

    Setup and driver code for 10

--- gfx-fnd-10/gfx-fnd-10/main.swift
+++ gfx-fnd-10/gfx-fnd-10/main.swift
@@ -8,5 +8,48 @@
 
 import Foundation
 
-print("Hello, World!")
+func Usage()
+{
+  let exec = CommandLine.arguments[0]
+  print ("\(exec.components(separatedBy: "/").last ?? exec) <inputfile> <outputfile>")
+}
+
+func readArgs() -> (inputFile:String, outputFile:String?)
+{
+  guard let inputFile = CommandLine.arguments[safe: 1]
+    else
+  {
+    Usage()
+    exit (1)
+  }
+  
+  if (!FileManager.default.fileExists(atPath: inputFile))
+  {
+    print ("Input file does not exist: \(inputFile)")
+    exit(1)
+  }
+  
+  let outputFile = CommandLine.arguments[safe:2]
+  
+  return (inputFile, outputFile)
+  
+}
+
+func main()
+{
+  
+  let (inputFile, outputFile) = readArgs()
+  let jsonInput = JSON(data: read(pathAndFilename: inputFile).data(using: .ascii)!)
+
+  print (jsonInput.count)
+  
+  PNMImage(type: .RGB, width: 100, height: 100, maxScale: 255, pixels: []).write(toFile: outputFile)
+
+}
+
+
+main()
+
+
+
 
