e22d5858fdbc51f483541cb7f9dbc89c52d8f73e
asu <arsenyk.ustaris.dev@gmail.com>
2017-01-29 15:32:21 -0800

    New project for 11

---
+++ gfx-fnd-11/gfx-fnd-11/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  gfx-fnd-11
+//
+//  Created by asu on 2017-01-29.
+//  Copyright © 2017 ArsenykUstaris. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
